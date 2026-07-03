16fc7f79db140a994f3184e1f986af8b8df8e85f
rifki ramdani <ramdanirifki45@gmail.com>
2026-07-03 18:12:25 +0700

    fix 1.5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-    })->create();
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
