684eceadfdf89a814dad5fba13dfaac3de09e971
rifki ramdani <ramdanirifki45@gmail.com>
2026-07-03 18:55:01 +0700

    fix 1.9

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-  <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+  <div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
     <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden">
       {{ $slot }}
     </div>
